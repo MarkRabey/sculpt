8809b930934be38d16e3c7b28c27f7b498ae889f
Mark Rabey <mark@markrabey.com>
2017-11-03 10:49:56 -0400

    updated build process, added banner to css

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,22 @@
 'use strict'
 
+const pkg = require('./package.json');
+const banner = `Sculpt v${ pkg.version }\nCopyright 2017 ${ pkg.author }\nLicensed under MIT (https://github.com/MarkRabey/sculpt/blob/master/LICENSE)`;
+
 module.exports = (ctx) => ({
+  use: [
+    'postcss-banner',
+  ],
   map: ctx.file.dirname.includes('examples') ? false : {
     inline: false,
     annotation: true,
-    sourcesContent: true
+    sourcesContent: true,
   },
   plugins: {
-    autoprefixer: { cascade: false }
+    autoprefixer: { cascade: false },
+    'postcss-banner': {
+      important: true,
+      banner,
+    },
   }
 });
