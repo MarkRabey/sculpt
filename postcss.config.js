910481228b340925f7610b882e3023e59a6ce197
Mark Rabey <mark@markrabey.com>
2017-11-03 11:57:33 -0400

    add colour variables, add normalize.css

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,6 @@ const pkg = require('./package.json');
 const banner = `Sculpt v${ pkg.version }\nCopyright 2017 ${ pkg.author }\nLicensed under MIT (https://github.com/MarkRabey/sculpt/blob/master/LICENSE)`;
 
 module.exports = (ctx) => ({
-  use: [
-    'postcss-banner',
-  ],
   map: ctx.file.dirname.includes('examples') ? false : {
     inline: false,
     annotation: true,
